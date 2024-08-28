d9bc207fd50f9d4e64a3d2b201e05db2e476d9e0
dimagamera <46674070+dimagamera@users.noreply.github.com>
2024-08-29 03:10:40 +0400

    Update app.py

--- functions/app.py
+++ functions/app.py
@@ -1,41 +1,38 @@
-import io
-import logging
-from PIL import Image
-from rembg import remove
+from flask import Flask, request, jsonify
 import base64
+from PIL import Image
+from io import BytesIO
+from rembg import remove
 
-# Налаштування логування
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+app = Flask(__name__)
 
-def handler(event, context):
+@app.route('/remove-background', methods=['POST'])
+def remove_background():
     try:
+        # Перевірте, чи є дані у запиті
+        data = request.get_json()
+        if 'body' not in data:
+            return jsonify({'error': 'No body in request'}), 400
+
         # Отримання зображення з запиту
-        base64_image = event['body']
+        base64_image = data['body']
         image_bytes = base64.b64decode(base64_image)
-        input_image = Image.open(io.BytesIO(image_bytes))
+        input_image = Image.open(BytesIO(image_bytes))
         input_image = input_image.convert("RGBA")
 
         # Видалення фону
         output_image = remove(input_image)
 
         # Збереження результату в буфер
-        output_buffer = io.BytesIO()
+        output_buffer = BytesIO()
         output_image.save(output_buffer, format='PNG')
         output_buffer.seek(0)
         output_base64 = base64.b64encode(output_buffer.getvalue()).decode('utf-8')
 
-        return {
-            'statusCode': 200,
-            'body': output_base64,
-            'isBase64Encoded': True,
-            'headers': {
-                'Content-Type': 'image/png'
-            }
-        }
+        return jsonify({'body': output_base64})
+
     except Exception as e:
-        logger.error(f'An error occurred: {e}')
-        return {
-            'statusCode': 500,
-            'body': str(e)
-        }
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
